21b3d731bf014e1238e2a3bff71d8cceec3c22cb
Gregory Kipchumba <gregory.k182@gmail.com>
2026-05-29 23:10:54 +0300

    updated app.py

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    CORS(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    @app.route("/")
+    def home():
+        return {
+            "message": "Book Review API is running"
+        }
+
+    return app
+
+app = create_app()
