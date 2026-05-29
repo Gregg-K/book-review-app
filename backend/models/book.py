517878ee152ff58e03ddfd7514ad61331b5277a2
Gregory Kipchumba <gregory.k182@gmail.com>
2026-05-29 23:24:16 +0300

    added book.py

---
+++ backend/models/book.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    reviews = db.relationship("Review", backref="book", lazy=True, cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "description": self.description,
+            "user_id": self.user_id
+        }
